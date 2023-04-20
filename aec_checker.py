3fbc735517c9f822f20bb3996379e8fdfe26646c
Owen Miller <owen9825@gmail.com>
2023-04-20 18:37:10 +1000

    Accommodating NationBuilder fields

--- aec_checker.py
+++ aec_checker.py
@@ -1,4 +1,8 @@
 #!python3
+import io
+import os
+from typing import Dict, Optional, Tuple
+
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
@@ -24,24 +28,71 @@ AECStatus = collections.namedtuple(
     "AECStatus", ["result", "federal", "state", "local_gov", "local_ward"]
 )
 
+ADDRESSES = {
+    "address1",
+    "address2",
+    "address3",
+    "city",  # Suburb
+    "state",
+    "zip",  # Postal code
+    "country_code"
+}
+
+PRIMARY_ADDRESSES = [f"primary_{val}" for val in ADDRESSES]
+
+EXPECTED_FIELDS = {
+    "first_name",
+    "middle_name",
+    "last_name",
+    "nationbuilder_id"
+}.union(PRIMARY_ADDRESSES)
+
+
+def get_given_names(membership_row: Dict[str, Optional[str]]):
+    return (membership_row["first_name"] + " " + membership_row["middle_name"]).strip()
+
+
+def get_address_components(row: Dict[str, Optional[str]]) -> Tuple[
+    Optional[str],Optional[str], Optional[str], Optional[str]]:
+    """
+    For a membership row, return the street name, suburb, state, and postcode.
+    """
+    street_words = []
+    for word in row["primary_address1"].split():
+        if street_words or len(word.strip("0123456789")) == len(word):
+            street_words.append(word)
+    street_name = " ".join(street_words)
+    return street_name, row["primary_city"], row["primary_state"], row["primary_zip"]
+
+
+CAPTCHA_INPUT_ID = "textVerificationCode"
+
+
+def focus_on_captcha_input(driver: webdriver):
+    driver.find_element(
+        By.ID, CAPTCHA_INPUT_ID
+    ).send_keys("")
+
 
 def getAECStatus(
     driver: webdriver,
-    givenNames: str,
-    surname: str,
-    postcode: str,
-    suburb: str,
-    state: str,
-    streetName: str,
+    membership_row: Dict[str, Optional[str]]
 ) -> AECStatus:
-
+    given_names = get_given_names(membership_row)
+    street, suburb, state, postcode = get_address_components(membership_row)
+    if not postcode or not postcode.isnumeric():
+        print(f"{given_names} lacks a postcode, so we lack valid details for them")
+        return AECStatus(
+            AECResult.FAIL, None, None, None, None
+        )
     elem = driver.find_element(By.ID, "ctl00_ContentPlaceHolderBody_textGivenName")
     elem.clear()
-    elem.send_keys(givenNames)
+    elem.send_keys(given_names)
 
     elem = driver.find_element(By.ID, "ctl00_ContentPlaceHolderBody_textSurname")
     elem.clear()
-    elem.send_keys(surname)
+    elem.send_keys(membership_row["last_name"])
+    print(f"Considering {given_names} {membership_row['last_name']} ({membership_row['nationbuilder_id']})")
 
     elem = driver.find_element(By.ID, "ctl00_ContentPlaceHolderBody_textPostcode")
     elem.clear()
@@ -61,21 +112,18 @@ def getAECStatus(
 
     elem = driver.find_element(By.ID, "ctl00_ContentPlaceHolderBody_textStreetName")
     elem.clear()
-    elem.send_keys(streetName)
+    elem.send_keys(street)
 
     captcha_entered = False
-
-    driver.find_element(
-        By.ID, "ctl00_ContentPlaceHolderBody_textVerificationCode"
-    ).send_keys("")
+    focus_on_captcha_input(driver)
 
     while not captcha_entered:
-        time.sleep(1)
         try:
             elem = driver.find_element(
-                By.ID, "ctl00_ContentPlaceHolderBody_textVerificationCode"
+                By.ID, CAPTCHA_INPUT_ID
             )
         except NoSuchElementException:
+            print("The browser must have been commandeered")
             break
 
         if len(elem.get_attribute("value")) == 4:
@@ -91,9 +139,9 @@ def getAECStatus(
                 captcha_entered = True
 
             if not captcha_entered:
-                driver.find_element(
-                    By.ID, "ctl00_ContentPlaceHolderBody_textVerificationCode"
-                ).send_keys("")
+                focus_on_captcha_input(driver)
+        else:
+            time.sleep(1)
 
     try:
         driver.find_element(By.ID, "ctl00_ContentPlaceHolderBody_panelSuccess")
@@ -142,10 +190,50 @@ def getAECStatus(
         return out
 
 
+OUTPUT_FIELDS = ["first_name", "middle_name", "last_name", "nationbuilder_id",
+                 "AEC_result", "federal_division", "state_division", "local_government", "local_ward"]
+
+
 def get_driver():
     # https://github.com/SergeyPirogov/webdriver_manager#use-with-firefox
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     return driver
+
+
+def check_rows(input_filename, output_filename, skip: int):
+    with get_driver() as driver:
+        driver.get("https://check.aec.gov.au/")
+        with io.open(input_filename) as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=",")
+            if not EXPECTED_FIELDS.issubset(reader.fieldnames):
+                raise ValueError(f"Some fields are missing from this file: one of {', '.join(EXPECTED_FIELDS)}")
+            row_count = 0
+            existing_output = os.path.exists(output_filename)
+            with io.open(
+                output_filename,
+                "a",
+                newline="",
+            ) as output_file:
+                writer = csv.DictWriter(output_file, fieldnames=OUTPUT_FIELDS)
+                if not existing_output:
+                    writer.writeheader()
+                for membership_row in reader:
+                    row_count += 1
+                    output_row = {k: membership_row.get(k) for k in OUTPUT_FIELDS}
+                    if row_count <= skip:
+                        # Assume that this has already been written as output.
+                        continue
+                    if not membership_row["first_name"]:
+                        # A member needs a  name
+                        continue
+                    time.sleep(0.1)
+                    status = getAECStatus(driver, membership_row)
+                    output_row.update({"AEC_result": status[0],
+                                       "federal_division": status[1],
+                                       "state_division": status[2],
+                                       "local_government": status[3],
+                                       "local_ward": status[4]})
+                    writer.writerow(output_row)
 
 
 def main():
@@ -155,31 +243,12 @@ def main():
     parser.add_argument(
         "--skip", type=int, default=0, help="skip entries you've already seen"
     )
-    parser.add_argument("--infile", default="input.csv")
+    parser.add_argument("--infile", default="input.csv",
+                        help="This file is presumed to be exported from NationBuilder, with fields such as 'first_name'"
+                             " and 'primary_address1'")
     parser.add_argument("--outfile", default="output.csv")
     args = parser.parse_args()
-
-    with get_driver() as driver:
-        driver.get("https://check.aec.gov.au/")
-
-        with csv.writer(
-            open(
-                args.outfile,
-                "a",
-                newline="",
-            )
-        ) as writer:
-            count = 0
-            with open(args.infile) as csvfile:
-                reader = csv.reader(csvfile, delimiter=",")
-                for row in reader:
-                    count += 1
-                    if count <= 1 + args.skip:
-                        writer.writerow(row)
-                        continue
-                    time.sleep(0.1)
-                    status = getAECStatus(driver, *row[:6])
-                    writer.writerow(row + [i for i in status])
+    check_rows(args.infile, args.outfile, args.skip)
 
 
 if __name__ == "__main__":
